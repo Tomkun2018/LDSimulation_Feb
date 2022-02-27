40dcd1834e08369754353e9dd586943813f2518e
yhdis <41714075+davidysding@users.noreply.github.com>
2022-02-26 22:49:41 -0500

    LevelLoader Done + Minor Adjustment

--- Assets/Scripts/SceneLoader/SceneLoadTrigger.cs
+++ Assets/Scripts/SceneLoader/SceneLoadTrigger.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+
 public class SceneLoadTrigger : MonoBehaviour
 {
-    public string targetScene;
+    public string targetScene = null;
+    private string currenPressed = null;
+    private string presceneName = "level";
+
     public void LoadScene()
     {
-        LoadingData.sceneToLoad = targetScene;
+        currenPressed = EventSystem.current.currentSelectedGameObject.name;
+        //Debug.Log(currenPressed);
+        LoadingData.sceneToLoad = presceneName + currenPressed.Substring(5);
         SceneManager.LoadScene("LoadingScene");
     }
 }
