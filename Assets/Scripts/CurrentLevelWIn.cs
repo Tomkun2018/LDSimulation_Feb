75d78bb4f5a26692aca2f1876c2cb54aa422eafb
Tomkun2018 <62909320+Tomkun2018@users.noreply.github.com>
2022-02-27 21:56:58 -0500

    Finalized 3 levels

--- Assets/Scripts/CurrentLevelWIn.cs
+++ Assets/Scripts/CurrentLevelWIn.cs
@@ -6,23 +6,15 @@ public class CurrentLevelWIn : MonoBehaviour
 {
     public GameObject btn;
     public GameObject timer;
+    public int number;
     private void Awake()
     {
-        btn = GameObject.Find("NextLevelBtn");
+       
         timer = GameObject.Find("Timer");
     }
     private void OnTriggerEnter(Collider other)
     {
 
-        Debug.Log(other);
-        if ( other.gameObject.name == "Player")
-        {
-            Vector3 pos = btn.transform.position;
-            pos.x += 50f;
-            pos.y -= 50f; 
-            btn.transform.position = pos;
-            btn.transform.localScale = new Vector3(2,2,2);
-        }
-        timer.GetComponent<Timer>().enabled = false;
+        btn.GetComponent<Loader>().Load(number);
     }
 }
